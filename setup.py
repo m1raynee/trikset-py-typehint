6913c809fed7f36094de2ea5115d012853572a08
m1raynee <pechenko-7@ya.ru>
2022-01-20 22:05:05 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="trikset-py-typehint",
-    version="0.0.1",
+    version="0.0.2",
     author="m1raynee",
     author_email="pechenko-7@ya.ru",
     description="A package that provides type signatures to all documented methods",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/m1raynee/trikset-py-typehint",
     project_urls={
         "Bug Tracker": "https://github.com/m1raynee/trikset-py-typehint/issues",
